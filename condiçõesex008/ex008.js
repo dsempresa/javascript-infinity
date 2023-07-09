1b4d65be51390d1e2fb9c920eab824a11735ae0c
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-09 02:26:49 -0300

    exercício 008

---
+++ condiçõesex008/ex008.js
@@ -0,0 +1,22 @@
+
+let resultado = document.querySelector('#res')
+
+
+function calcular() {
+    
+    let idade = document.querySelector('#iidade').value
+    let nome = document.querySelector('#inome').value
+        if(idade > 0 && idade <= 10) {
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade} anos ficará o valor de R$30,00.`
+    } if(idade > 10 && idade <= 29) {
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade} anos ficará o valor de R$60,00.`
+    } if(idade > 29 && idade <= 45) {
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade} anos ficará o valor de R$120,00.`
+    } if(idade > 45 && idade <= 59) {
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade} anos ficará o valor de R$150,00.`
+    } if(idade > 59 && idade <= 65) {
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade} anos ficará o valor de R$250,00.`
+    } else{
+        resultado.innerHTML = `${nome}, seu plano nessa idade de ${idade}`
+    }
+}
