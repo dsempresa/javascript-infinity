37ab6c183bff8b3a31dbcf4e0adcc42ee0eacf5f
dsempresa <130731804+dsempresa@users.noreply.github.com>
2023-07-11 02:31:21 -0300

    Concluído tema - condições

---
+++ exemplos/exemplo.js
@@ -0,0 +1,16 @@
+switch (new Date().getDay()) { //Retorna o dia da semana como um int de 0-6
+    case 0:
+    day = "Domingo"; break;
+    case 1:
+    day = "Segunda-feira"; break;
+    case 2:
+    day = "Terça-feira"; break;
+    case 3:
+    day = "Quarta-feira"; break;
+    case 4:
+    day = "Quinta-feira"; break;
+    case 5:
+    day = "Sexta-feira"; break;
+    case 6:
+    day = "Sábado";
+}
