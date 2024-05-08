9b69f3ab166be456d3be080facc188643253e6dc
dsempresa <130731804+dsempresa@users.noreply.github.com>
2024-05-07 22:34:08 -0300

    Create prova003.js

---
+++ prova_aula003js/prova003.js
@@ -0,0 +1,25 @@
+// Você foi contratado(a) para desenvolver um programa que irá auxiliar um professor a calcular algumas estatísticas das notas de seus alunos. O programa deve solicitar ao professor o número total de estudantes na turma e, em seguida, pedir que ele insira as notas de cada aluno individualmente. Após receber todas as notas, o programa deverá calcular a média da turma e identificar a maior e a menor nota obtida.
+
+
+
+// Instruções:
+
+// Solicite ao professor que digite o número total de estudantes na turma.
+// Em seguida, peça que o professor insira a nota de cada aluno individualmente, uma por vez.
+// Calcule a média da turma somando todas as notas e dividindo pelo número total de estudantes.
+// Identifique e registre a maior nota obtida na turma.
+// Ao final, exiba a média da turma e a maior e a menor nota encontrada.
+
+// Dicas:
+
+// Utilize um loop while para coletar as notas de todos os alunos.
+// Armazene as notas em uma variável e vá atualizando o valor da soma a cada nova nota inserida.
+// Compare cada nota com a maior nota atualmente registrada para encontrar a maior nota.
+// Para calcular a média, divida a soma das notas pelo número total de estudantes.
+// Exiba os resultados de forma clara e organizada.
+
+
+let totalEstudantes = parseInt(prompt("Digite o número total de estudantes na turma:"));
+
+// Exibir o número total de estudantes fornecido pelo professor
+console.log("O número total de estudantes na turma é: " + totalEstudantes);
